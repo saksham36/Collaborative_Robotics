2878dd6a1fa62102f0511e20668bbfac775d5db4
Carlota Parés-Morlans <cpares@stanford.edu>
2023-03-09 02:57:30 -0800

    goal finder

--- me326_locobot_example/scripts/perception/goal_finder.py
+++ me326_locobot_example/scripts/perception/goal_finder.py
@@ -176,10 +176,8 @@ class GoalFinder:
 
     
     def get_xy_from_cell_index(self, index):
-        rospy.loginfo("origin: {}".format(self.perception_grid.info.origin.position))
-        rospy.loginfo("resolution: {}".format(self.perception_grid.info.resolution))
-        x = index[1]*self.perception_grid.info.resolution + self.perception_grid.info.origin.position.x
-        y = index[0]*self.perception_grid.info.resolution + self.perception_grid.info.origin.position.y
+        x = index[0]*self.perception_grid.info.resolution + self.perception_grid.info.origin.position.x
+        y = index[1]*self.perception_grid.info.resolution + self.perception_grid.info.origin.position.y
         return x, y
 
     def get_grid_as_np(self, grid):
